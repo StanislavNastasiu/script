f5991387612c2825f013979b9d94a0cd4624f7a6
stanislav.nastasiu <stanislav.nastasiu@retailsolutions.it>
2019-12-03 09:20:09 +0100

    rimossi file aevi ecc

--- AeviPay/X.Pay_Service_LAN/Service_1.0.4.12/RestartService.bat
+++
@@ -1,5 +0,0 @@
-set _ServiceName=XPaySocketTunnel
-
-sc query %_ServiceName% | find "does not exist" >nul
-if %ERRORLEVEL% EQU 0 echo Service Does Not Exist.
-if %ERRORLEVEL% EQU 1 net stop %_ServiceName% && net start %_ServiceName%
